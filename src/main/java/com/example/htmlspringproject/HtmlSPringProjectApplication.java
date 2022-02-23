2a7b94166eab28c7432032f953f15bd19c3d0333
SebastianBueBjorner <seba169m@stud.kea.dk>
2022-02-23 20:07:27 +0100

    First version

---
+++ src/main/java/com/example/htmlspringproject/HtmlSPringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlSPringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlSPringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/htmlspringproject/HtmlSPringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmlspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlSPringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
